bf0f26cc02926ff42b3b9864e575d8cac349bb03
Mendrejk <bastian.lakomy@gmail.com>
2026-05-07 21:46:27 +0200

    feat: adapt to headless server layout (remove GUI, add zellij)

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,15 +1,5 @@
 starship init fish | source
 
-function toggle-starship-theme
-    if grep -q "palette = 'gruvbox_light'" ~/.config/starship.toml
-        sed -i "s/palette = 'gruvbox_light'/palette = 'gruvbox_dark'/" ~/.config/starship.toml
-        echo "Switched to Gruvbox Dark"
-    else
-        sed -i "s/palette = 'gruvbox_dark'/palette = 'gruvbox_light'/" ~/.config/starship.toml
-        echo "Switched to Gruvbox Light"
-    end
-end
-
 # Replace ls with eza
 alias ls='eza --icons'
 alias ll='eza -l --header --icons'
@@ -18,3 +8,8 @@ alias tree='eza --tree'
 
 zoxide init fish | source
 alias cd='z'
+
+# Load custom secrets (e.g., GH API key) if present
+if test -f ~/.config/fish/conf.d/secrets.fish
+    source ~/.config/fish/conf.d/secrets.fish
+end
